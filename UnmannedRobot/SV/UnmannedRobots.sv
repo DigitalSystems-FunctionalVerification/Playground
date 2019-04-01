f09d8af352fd29bca443b53cd810e950aef4e9ae
DarlanJurak <darlan.jurak@gmail.com>
2019-04-01 13:19:05 -0300

    added Reverse Sorting by struct method and changed class robot to be virtual

--- UnmannedRobot/SV/UnmannedRobots.sv
+++ UnmannedRobot/SV/UnmannedRobots.sv
@@ -7,12 +7,14 @@ package UnmannedRobots;
 
     } Device_s;
 
-    class Robot;
+    virtual class Robot;
         
         // Class members
         string      name;
         Device_s    devices[];  // Dynamic array declaration
         string      mission;
+
+        int  efficiency = 1;
         
         // Class constructor
         function new(string name = "", Device_s devices[] = '{}, string mission = "exist");
@@ -25,12 +27,20 @@ package UnmannedRobots;
         end          
         endfunction
 
+        function int get_efficiency();
+        begin
+            return this.efficiency;
+        end
+        endfunction;
+
         // Function in class
         function void display();
         begin
+            $write("################################################################\n");
             $write("Robot name: %s \n"                          , this.name                                 );        
             $write("Robot amount of devices: %d \n"             , $size(this.devices)                       );
             foreach (this.devices[i]) $write("Device %d: %s \n" , this.devices[i].id, this.devices[i].taskk );
+            $write("My efficiency is: %d \n"                    , this.efficiency);
             $display;            
         end
         endfunction
@@ -39,6 +49,7 @@ package UnmannedRobots;
         begin
             $write("Robot: %s \n", this.name);
             $write("My mission is to %s.\n", this.mission);
+            $write("################################################################\n");
             $display;
         end
         endfunction
@@ -50,6 +61,13 @@ package UnmannedRobots;
         function new(string name, Device_s devices[] = '{}, string mission = "fly through the skies");
         begin
             super.new(name, devices, mission);
+            this.efficiency = 2;
+        end
+        endfunction
+
+        function void display();
+        begin
+            super.display();
         end
         endfunction
 
@@ -60,6 +78,13 @@ package UnmannedRobots;
         function new(string name, Device_s devices[] = '{}, string mission = "explore the ground surface");
         begin
             super.new(name, devices, mission);
+            this.efficiency = 3;
+        end
+        endfunction
+
+        function void display();
+        begin
+            super.display();
         end
         endfunction
 
@@ -71,6 +96,13 @@ package UnmannedRobots;
         function new(string name, Device_s devices[] = '{}, string mission = "navigate through the oceans");
         begin
             super.new(name, devices, mission);
+            this.efficiency = 4;
+        end
+        endfunction
+
+        function void display();
+        begin
+            super.display();
         end
         endfunction
 
--- UnmannedRobot/SW/test.sv
+++ UnmannedRobot/SW/test.sv
@@ -7,24 +7,44 @@ program test;
         
         Device_s devices[] = '{ '{0, "Localization"}, 
                                 '{1, "Motion"}};
-        Robot                   robot;
+        // Robot                   robot;
         UnmannedAerialVehicle   uav;
         UnmannedGroundVehicle   ugv;
         UnmannedMarineVehicle   umv;
 
-        robot = new("robot1", devices);
+        Robot robots[$];
+
+        // robot = new("rbt1", devices);
         // robot.display();
-        robot.define_mission();
+        // robot.define_mission();
 
         uav = new("uav1", devices);
+        uav.display();
         uav.define_mission();
 
         ugv = new("ugv1", devices);
+        ugv.display();
         ugv.define_mission();
 
         umv = new("umv1", devices);
+        umv.display();
         umv.define_mission();
 
+        // robots.push_front(robot);
+        
+        robots.push_front(ugv);
+        robots.push_front(umv);
+        robots.push_front(uav);
+
+        foreach (robots[i]) $write("Robot: %s Efficiency: %d\n", robots[i].name, robots[i].get_efficiency());
+        $write("################################################################ After Reverse Sorting\n");
+        $display();
+
+        robots.rsort with (item.get_efficiency());
+
+        foreach (robots[i]) $write("Robot: %s Efficiency: %d\n", robots[i].name, robots[i].get_efficiency());
+        $write("################################################################\n");
+        $display();
 
     end
     
